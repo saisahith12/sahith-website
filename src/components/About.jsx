11e0ac87961702e0476d79f72247b16465ddc3ea
saisahith12 <saisahith1711@gmail.com>
2024-07-01 15:10:50 +0530

    Commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import aboutImg from "../assets/SahithPhoto.jpg";
 import { ABOUT_TEXT } from "../constants";
-import { motion } from "framer-motion";
+import motion from "framer-motion";
 
 const About = () => {
   return (
